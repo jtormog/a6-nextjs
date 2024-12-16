56b2ee10acbe6cc2aa7a9e4a9f4edf3c3a7aee21
Josep Tormo <jostorgar4@alu.edu.gva.es>
2024-12-16 17:04:03 +0100

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,11 @@
-export default function pageDashboard(){
+import { db } from '@vercel/postgres'
+
+const client = await db.connect();
+
+export default async function pageDashboard(){
+    
+    const  
+
     return (
         <p>Home: /dashboard</p>
     );
---
+++ app/seed/route.ts
@@ -0,0 +1,11 @@
+import { db } from '@vercel/postgres'
+
+const client =  await db.connect();
+
+export async function GET(){
+    await client.sql `BEGIN`
+    await client.sql `CREATE TABLE A6_USERS (name text)`
+    await client.sql `INSERT INTO A6_USERS (name) VALUES ('Jose Luis'), ('Carmen Mari)`
+    await client.sql `COMMIT`
+    return Response.json({message: 'Pues ya estaría'})
+}
